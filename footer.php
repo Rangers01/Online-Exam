865af4383dd9136882e89cc8cf057f800f5a1314
Rangers01 <43227659+Rangers01@users.noreply.github.com>
2019-03-31 02:17:10 +0530

    online exam system

---
+++ footer.php
@@ -0,0 +1,3 @@
+ <div id="footer">
+        &copy <?php echo date("Y");?><p style="font-size:70%;color:#ffffff;"> Developed By-<b>RENGERS</b></p>
+      </div>
---
+++ header.php
@@ -0,0 +1,3 @@
+<div class="header">
+                <img style="margin:10px 2px 2px 10px;float:left;" height="80" width="200" src="images/logo.png" alt="OES"/><h3 class="headtext"> &nbsp;Online Examination System </h3><h4 style="color:#ffffff;text-align:center;margin:0 0 5px 5px;"><i>...because Examination Matters</i></h4>
+            </div>
